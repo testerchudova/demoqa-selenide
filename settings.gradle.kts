977b6949096846bd728e9eadfe6d1e0e4f0b04f7
Ekaterina Chudova <testerchudova@gmail.com>
2026-02-28 14:49:25 +0300

    Refactor project structure: move homework to root src

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "demoqa-selenide"
-
+include("PrimitiveOperations")
--- src/main/java/org/example/PrimitiveOperations.java
+++ src/main/java/org/example/PrimitiveOperations.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class PrimitiveOperations {
 
     public static void main(String[] args) {
