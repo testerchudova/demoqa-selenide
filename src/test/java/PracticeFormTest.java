f58eaa76fb6a948aa176e9a5d2a046ce5e479de2
Ekaterina Chudova <testerchudova@gmail.com>
2026-03-03 11:29:23 +0300

    Refactor PracticeFormTest according to review comments

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,39 +1,55 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import org.junit.jupiter.api.BeforeEach;
-import static com.codeborne.selenide.Condition.visible;
-
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Condition.exist;
 
 public class PracticeFormTest {
 
     @BeforeAll
-    static void setup() {
+    static void setupSelenideConfig() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.screenshots = true;
         Configuration.savePageSource = true;
-
     }
 
     @BeforeEach
-    void openForm() {
+    void openPracticeForm() {
         open("/automation-practice-form");
+        executeJavaScript("""
+                document.getElementById('fixedban')?.remove();
+                document.querySelector('footer')?.remove();
+                """);
     }
-// Позитивные 1,2
-    @Test
-    void fillAllFields_submit_success() {
 
+    // Позитивные 1,2
+    @Test
+    void fillAllFieldsTest() {
         $("#firstName").setValue("Katya");
         $("#lastName").setValue("Ivanova");
         $("#userEmail").setValue("katya@test.com");
         $(byText("Female")).click();
         $("#userNumber").setValue("1234567890");
-        $("#dateOfBirthInput").setValue("10 Oct 1995").pressEnter();
+
+        $("#dateOfBirthInput").scrollTo().click();
+        $(".react-datepicker").shouldBe(visible);
+        $(".react-datepicker__month-select").shouldBe(visible).click();
+        $(".react-datepicker__month-select option[value='9']").click();   // October
+        $(".react-datepicker__year-select").shouldBe(visible).click();
+        $(".react-datepicker__year-select option[value='1995']").click();
+        $(".react-datepicker__day--010:not(.react-datepicker__day--outside-month)").click();
+        $("#dateOfBirthInput").shouldHave(value("10 Oct 1995"));
+
         $("#subjectsInput").setValue("Maths").pressEnter();
         $(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("testpic.jpg");
@@ -42,34 +58,66 @@ public class PracticeFormTest {
         $(byText("NCR")).click();
         $("#city").click();
         $(byText("Delhi")).click();
-
         $("#submit").click();
+
+        //Проверки по каждому полю
+
+        $(".table-responsive").shouldBe(visible);
+        $(".table-responsive").shouldHave(text("Student Name"));
+        $(".table-responsive").shouldHave(text("Katya Ivanova"));
+        $(".table-responsive").shouldHave(text("Student Email"));
+        $(".table-responsive").shouldHave(text("katya@test.com"));
+        $(".table-responsive").shouldHave(text("Gender"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("Mobile"));
+        $(".table-responsive").shouldHave(text("1234567890"));
+        $("#dateOfBirthInput").shouldHave(value("10 Oct 1995"));
+        $(".table-responsive").shouldHave(text("Subjects"));
+        $(".table-responsive").shouldHave(text("Maths"));
+        $(".table-responsive").shouldHave(text("Hobbies"));
+        $(".table-responsive").shouldHave(text("Reading"));
+        $(".table-responsive").shouldHave(text("Picture"));
+        $(".table-responsive").shouldHave(text("testpic.jpg"));
+        $(".table-responsive").shouldHave(text("Address"));
+        $(".table-responsive").shouldHave(text("Moscow, Arbat 1"));
+        $(".table-responsive").shouldHave(text("State and City"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
+
         $(".modal-content").shouldBe(visible);
-        $("#example-modal-sizes-title-lg").shouldHave(com.codeborne.selenide.Condition.text("Thanks for submitting the form"));
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
     }
 
     @Test
-    void fillMandatoryFields_submit_success() {
-
+    void fillMandatoryFieldsTest() {
         $("#firstName").setValue("Katya");
         $("#lastName").setValue("Ivanova");
         $(byText("Female")).click();
         $("#userNumber").setValue("1234567890");
 
         $("#submit").click();
+
         $(".modal-content").shouldBe(visible);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table-responsive").shouldBe(visible);
+        $(".table-responsive").shouldHave(text("Student Name"));
+        $(".table-responsive").shouldHave(text("Katya Ivanova"));
+        $(".table-responsive").shouldHave(text("Gender"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("Mobile"));
+        $(".table-responsive").shouldHave(text("1234567890"));
     }
 
-//Пустая форма не отправляется - Негативный 1
-@Test
-void submitEmptyForm_shouldNotShowModal() {
-    $("#submit").click();
-    $(".modal-content").shouldNotBe(visible);
-}
+    //Пустая форма не отправляется - Негативный 1
+    @Test
+    void submitEmptyForm_shouldNotShowModal() {
+        $("#submit").click();
+        $(".modal-content").shouldNotBe(visible);
+    }
 //Ошибка в email - Негативный 2
 
     @Test
-    void invalidEmail_shouldNotSubmit() {
+    void invalidEmailShouldNotSubmitTest() {
         $("#firstName").setValue("Katya");
         $("#lastName").setValue("Ivanova");
         $("#userEmail").setValue("katya-at-test.com");
@@ -77,27 +125,36 @@ void submitEmptyForm_shouldNotShowModal() {
         $("#userNumber").setValue("1234567890");
 
         $("#submit").click();
+
+        $("#userEmail").shouldHave(value("katya-at-test.com"));
+        $(".modal-content").shouldNotBe(visible);
+
+    }
+
+    //Ошибка в номере телефона по кол. символов - Негативный 3
+    @Test
+    void shortMobileShouldNotSubmitTest() {
+        $("#firstName").setValue("Katya");
+        $("#lastName").setValue("Ivanova");
+        $(byText("Female")).click();
+        $("#userNumber").setValue("12345");
+
+        $("#submit").click();
+
+        $("#userNumber").shouldHave(value("12345"));
         $(".modal-content").shouldNotBe(visible);
     }
-//Ошибка в номере телефона по кол. символов - Негативный 3
-@Test
-void shortMobile_shouldNotSubmit() {
-    $("#firstName").setValue("Katya");
-    $("#lastName").setValue("Ivanova");
-    $(byText("Female")).click();
-    $("#userNumber").setValue("12345");
 
-    $("#submit").click();
-    $(".modal-content").shouldNotBe(visible);
-}
     //Отсутсвует гендер - Негативный 4
     @Test
-    void withoutGender_shouldNotSubmit() {
+    void withoutGenderShouldNotSubmitTest() {
         $("#firstName").setValue("Katya");
         $("#lastName").setValue("Ivanova");
         $("#userNumber").setValue("1234567890");
 
         $("#submit").click();
+
+        $("input[name='gender']:checked").shouldNot(exist);
         $(".modal-content").shouldNotBe(visible);
     }
 }
