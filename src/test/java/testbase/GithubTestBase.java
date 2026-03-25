a968855b8e18689bd3fdb9fe1c1e8e3ad92306d8
Ekaterina Chudova <testerchudova@gmail.com>
2026-03-25 10:47:36 +0300

    del baseUrl

--- src/test/java/testbase/GithubTestBase.java
+++ src/test/java/testbase/GithubTestBase.java
@@ -10,6 +10,5 @@ public class GithubTestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadTimeout = 60000;
         Configuration.timeout = 10000;
-        Configuration.baseUrl = "";
-    }
+           }
 }
